d524d7ae1839fe73eaccd7cf160971f14cd083c3
Luai Saif <eng.luaish@gmail.com>
2018-10-16 16:23:09 +0800

    Change Entery page event algorithm

--- src/index.js
+++ src/index.js
@@ -13,25 +13,12 @@ smoothscroll.polyfill();
 
 ReactDOM.render(<EnteryPage />, document.getElementById("entry-page"));
 
-ReactDOM.render(<App />, document.getElementById("root"), () => {
-  console.log("RENDERED");
-  document.getElementById("entry-page").outerHTML = "";
-  var app = document.getElementById("app");
-  app.classList.remove("u-hide");
-  // setTimeout(function() {
-  // }, 3000);
-});
+ReactDOM.render(<App />, document.getElementById("root"));
 
-// window.addEventListener("load", event => {
-//    alert("press");
-//   console.log(event);
-//   console.log("All resources finished loading!");
-//   setTimeout(() => {
-//     ReactDOM.render(
-//       <App isPageLoading={false} />,
-//       document.getElementById("root")
-//     );
-//   }, 2000);
-// });
+window.addEventListener("load", () => {
+  document.getElementById("entry-page").outerHTML = "";
+  const app = document.getElementById("app");
+  app.classList.remove("u-hide");
+});
 
 registerServiceWorker();
